4c624f5e15ee49a4a88b32a3399fff96bee22439
Melody Davis <piano.fan122@gmail.com>
2022-10-11 12:35:01 -0400

    can now replace words with blanks

--- pages/game.tsx
+++ pages/game.tsx
@@ -10,6 +10,7 @@ const Game: NextPage = () => {
     const [songTitle, setSongTitle] = useState<string>('')
     const [songUrl, setSongUrl] = useState<string>('')
     const [lyrics, setLyrics] = useState<string[]>([])
+    const [answer, setAnswer] = useState<string>('')
     const [timestamps, setTimestamps] = useState<string[]>([])
     const [lyricIndex, setLyricIndex] = useState<number>(0)
     const [currentLyrics, setCurrentLyrics] = useState<string>('')
@@ -21,9 +22,26 @@ const Game: NextPage = () => {
                 setSongTitle(result.title)
                 setSongUrl(result.url)
                 setLyrics(result.lyrics)
+                replaceLyricWithBlanks(result.lyrics)
                 setTimestamps(result.timestamps)
             })
     }, [])
+
+    const replaceLyricWithBlanks = (lyrics: any) => {
+        const lastLineIndex = lyrics.length - 1
+
+        const correctAnswer = lyrics[lastLineIndex]
+
+        setAnswer(correctAnswer)
+
+        // regex captures all but first word
+        const blankLyricsRegex = /(^\S*\s*)?\S/g
+
+        const blankLyrics = correctAnswer.replace(blankLyricsRegex, '$1_')
+
+        lyrics.pop()
+        lyrics.push(blankLyrics)
+    }
 
     const timeUpdate = (e: any) => {
         const currentTime = e.target.currentTime
@@ -33,6 +51,14 @@ const Game: NextPage = () => {
             setCurrentLyrics(lyrics[lyricIndex])
             setLyricIndex(lyricIndex + 1)
         }
+
+        if (lyricIndex == lyrics.length) {
+            // this should pause the song
+        }
+    }
+
+    const revealAnswer = () => {
+        setCurrentLyrics(answer)
     }
 
     return (
@@ -41,8 +67,9 @@ const Game: NextPage = () => {
             <audio src={songUrl} onTimeUpdate={(e) => timeUpdate(e)} controls />
             <h2 className={styles.currentLyrics}>{currentLyrics}</h2>
             <div className={styles.gameButtonsDiv}>
+                <button className={styles.gameButtons} onClick={revealAnswer}>Submit Answer</button>
                 <Link href="/select">
-                    <a className={styles.gameButtons}>Submit answer</a>
+                    <a className={styles.gameButtons}>Next Song</a>
                 </Link>
                 <Link href="/scores">
                     <a className={styles.gameButtons}>Abandon Game</a>
